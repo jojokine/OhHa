93adc85e56d6014377a4499e73d6a89cb8b9be21
J Oskari Jokinen <jojokine@melkki.cs.helsinki.fi>
2012-09-04 14:57:58 +0300

    lisäiltiin alustavia luokkia projektiin

---
+++ projekti/muistipeli/kentta.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jojokine
+ */
+public class kentta {
+    private int kaannot;
+    private int parit;
+    private laatta [] laatat;
+    
+}
---
+++ projekti/muistipeli/muistiPeli.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jojokine
+ */
+public class muistiPeli {
+    
+    private int vaikeus;
+    private Kentta kentta;
+    
+}
