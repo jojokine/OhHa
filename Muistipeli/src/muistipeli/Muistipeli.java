31dfd332417d1b9c6f67a612c85a1555f4edddf9
Osku <osku@osku-Q330.(none)>
2012-09-12 12:19:23 +0300

    ohjelmoitu pääluokkaa ja testejä sille

--- Muistipeli/src/muistipeli/Muistipeli.java
+++ Muistipeli/src/muistipeli/Muistipeli.java
@@ -10,8 +10,26 @@ package muistipeli;
  */
 public class Muistipeli {
 
-    private int vaikeus;
-    
+//    private int vaikeus;
+//    private int kaannetty1;
+//    private int kaannetty2;
+
+//    private boolean onkoPari(Kentta peli) {
+//        if 
+//      {
+//        }
+//    }
+
+    public static boolean voikoKaantaa(Kentta peli, int paikka) {
+        if (peli.getKaannetyt() == 0) {
+            return true;
+        } else if (peli.getKaannetyt() == 1 && peli.getLaatat().get(paikka).getTila() == false) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
---
+++ Muistipeli/test/Testit/MuistipeliTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Testit;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author osku
+ */
+public class MuistipeliTest {
+
+    muistipeli.Kentta peli;
+
+    @Before
+    public void setUp() {
+        peli = new muistipeli.Kentta(1);
+    }
+
+    @Test
+    public void voikoKaantaaEiAnnaKaantaaSamaa() {
+        assertTrue(muistipeli.Muistipeli.voikoKaantaa(peli, 5));
+        peli.getLaatat().get(5).nayta();
+        peli.lisaaKaannettyja();
+        assertFalse(muistipeli.Muistipeli.voikoKaantaa(peli, 5));
+        peli.getLaatat().get(5).piilota();
+        peli.nollaaKaannetyt();
+        assertTrue(muistipeli.Muistipeli.voikoKaantaa(peli, 5));
+    }
+    
+    @Test
+    public void voikoKaantaaEiAnnaKaantaaKahtaEnempaa(){
+        assertTrue(muistipeli.Muistipeli.voikoKaantaa(peli, 5));
+        peli.getLaatat().get(5).nayta();
+        peli.lisaaKaannettyja();
+        assertTrue(muistipeli.Muistipeli.voikoKaantaa(peli, 6));
+        peli.getLaatat().get(6).nayta();
+        peli.lisaaKaannettyja();
+        assertFalse(muistipeli.Muistipeli.voikoKaantaa(peli, 7));
+        
+    }
+}
